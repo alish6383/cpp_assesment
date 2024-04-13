6e5739a207a29c531f45514d8e1e4774a61f6b19
alish6383 <126838727+alish6383@users.noreply.github.com>
2024-04-13 22:17:00 +0545

    Add files via upload

--- worksheet4/23000965_task2.cpp
+++ worksheet4/23000965_task2.cpp
@@ -1,99 +1,69 @@
 #include <iostream>
-using namespace std;
 
-class IntException {
-private:
-    int code;
-public:
-    IntException(int c) : code(c) {}
-    int getCode() const { return code; }
-};
+using namespace std;
 
 class Circle {
 private:
     float rad;
+
 public:
-    Circle(float r) {
-        if (r < 0)
-            throw IntException(10);
-        rad = r;
-    }
-    float getRadius() const { return rad; }
-    void setRadius(float r) { rad = r; }
+    Circle(float radius) : rad(radius) {}
+
     Circle& operator-(int n) {
-        float newRad = rad - n;
-        if (newRad < 0)
-            throw IntException(10);
-        rad = newRad;
+        rad -= n;
+        if (rad < 0)
+            throw 10;
         return *this;
+    }
+
+    void show() const {
+        cout << "Radius: " << rad << endl;
     }
 };
 
 class Ellipse : public Circle {
 private:
     float axis;
+
 public:
-    Ellipse(float r, float a) : Circle(r), axis(a) {}
-    float getAxis() const { return axis; }
-    void setAxis(float a) { axis = a; }
+    Ellipse(float radius, float ellipse_axis) : Circle(radius), axis(ellipse_axis) {}
+
     Ellipse& operator-(int n) {
         Circle::operator-(n);
         axis -= n;
         if (axis < 0)
-            throw IntException(20);
+            throw 20;
         return *this;
+    }
+
+    void show() const {
+        Circle::show();
+        cout << "Axis: " << axis << endl;
     }
 };
 
-Circle createCircle() {
-    float radius;
-    cout << "Enter the radius of the circle: ";
-    cin >> radius;
-    return Circle(radius);
-}
-
-Ellipse createEllipse() {
-    float radius, axis;
-    cout << "Enter the radius of the ellipse: ";
-    cin >> radius;
-    cout << "Enter the axis of the ellipse: ";
-    cin >> axis;
-    return Ellipse(radius, axis);
-}
-
-void f(Circle& c, int n) {
+void f(Circle &c, int n) {
     try {
-        c = c - n;
-        cout << "Result: Radius = " << c.getRadius() << endl;
-    } catch (IntException& e) {
-        cout << "Exception caught with code: " << e.getCode() << endl;
-    }
-}
-
-void f(Ellipse& e, int n) {
-    try {
-        e = e - n;
-        cout << "Result: Radius = " << e.getRadius() << ", Axis = " << e.getAxis() << endl;
-    } catch (IntException& e) {
-        cout << "Exception caught with code: " << e.getCode() << endl;
+        Circle tmp = c - n;
+        tmp.show();
+    } catch (int exception_code) {
+        if (exception_code == 10)
+            cout << "Exception: Circle radius cannot be negative." << endl;
+        else if (exception_code == 20)
+            cout << "Exception: Ellipse axis cannot be negative." << endl;
     }
 }
 
 int main() {
     try {
-        Circle cir = createCircle();
-        Ellipse ell = createEllipse();
-
-        Circle &r1 = cir;
-        Circle &r2 = ell;
-
+        Circle cir(5);
+        Ellipse ell(10, 6);
+        Circle &r1 = cir, &r2 = ell;
         f(r1, 3);
         f(r2, 1);
         f(r2, 10);
-    } catch (IntException& e) {
-        cout << "Exception caught with code: " << e.getCode() << endl;
     } catch (...) {
-        cout << "An unknown exception occurred" << endl;
+        cout << "An unknown exception occurred." << endl;
     }
 
     return 0;
